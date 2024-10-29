9b81982eff1c1800e24aed22901ca2f7003d8c59
屈彬策 <13154462+qu-bince@user.noreply.gitee.com>
2024-10-29 10:39:14 +0800

    clean

--- Programming/Chapter2/F_HeartBezier.cpp
+++ Programming/Chapter2/F_HeartBezier.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#define _USE_MATH_DEFINES
 #include <cmath>
+#include "Polynomial.h"          // Assuming we may need the Polynomial class
+#include "InterpCondition.h"     // and other classes if necessary
+#include "Interp.h"
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 // Function to generate points on the heart curve
 void generateHeartCurve(std::vector<double>& x_vals, std::vector<double>& y_vals, int m) {
     for (int i = 0; i <= m; ++i) {
         double t = M_PI * i / m;
         double x = 16 * pow(sin(t), 3);
-        double y = 13 * cos(t) - 5 * cos(2*t) - 2 * cos(3*t) - cos(4*t);
+        double y = 13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t);
         x_vals.push_back(x);
         y_vals.push_back(y);
     }
